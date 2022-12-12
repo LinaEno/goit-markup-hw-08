83637ed6d5a6b60877714d79dee824da860ec550
Lina Budkova <linabudkova35@gmail.com>
2022-12-12 17:08:45 +0200

    qweqwe

---
+++ js/01-gallery.js
@@ -0,0 +1,7 @@
+// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import galleryItems from './gallery-items.js'
+// Change code below this line
+
+console.log(galleryItems);
+
